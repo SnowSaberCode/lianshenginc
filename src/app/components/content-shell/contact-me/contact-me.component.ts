9e30086ad3cca92ad440584cf574e59e4e775983
SnowSaberCode <289655332@qq.com>
2024-05-03 14:57:17 +0800

    update style

--- src/app/components/content-shell/contact-me/contact-me.component.ts
+++ src/app/components/content-shell/contact-me/contact-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.less'
 })
--- src/app/components/content-shell/home-page/home-page.component.ts
+++ src/app/components/content-shell/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.less'
 })
-export default class HomePageComponent {
+export default class HomePageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+      var height = this.homePageImg.nativeElement.offsetHeight;
+      this.wordContent.nativeElement.style.top = height / 2 + 'px';
+  }
 
+  @ViewChild('homePageImg')
+  homePageImg!: ElementRef;
+  @ViewChild('wordContent')
+  wordContent!: ElementRef;
 }
